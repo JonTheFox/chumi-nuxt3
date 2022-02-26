ad7f9ae1dc082ca1b8abf017b83b768943f0b296
jonthefox <weissjon@gmail.com>
2022-02-26 20:19:03 +0200

    3rd commit

--- app.vue
+++ app.vue
@@ -3,3 +3,12 @@
     <h1>Oh yeah!</h1>
   </div>
 </template>
+<script setup>
+import { ref, onMounted } from "vue";
+
+const title = ref("");
+
+onMounted(() => {
+  title.value = "Updated!";
+});
+</script>
